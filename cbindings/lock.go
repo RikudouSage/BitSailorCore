b874f438a9e8b21e65200bedfa7ab223a78c40e2
Dominik Chrastecky <dominik@chrastecky.cz>
2026-07-26 11:59:05 +0200

    Add lock session

---
+++ cbindings/lock.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+#include "bw_common.h"
+*/
+import "C"
+import "go.chrastecky.dev/bitsailor-core/bitwarden/result"
+
+//export BitwardenLockSession
+func BitwardenLockSession(session C.SessionHandle) C.BitwardenStatus {
+	sessionGo, err := getHandleObj[*result.Session](handle(session))
+	if err != nil {
+		setLastError(err)
+		return BitwardenError
+	}
+
+	sessionGo.Encryption.UserKey = nil
+	clearLastError()
+	return BitwardenSuccess
+}
