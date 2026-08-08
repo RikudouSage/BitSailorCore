3e21a865f867d9139fde5ffdb8a8d71d1989e066
Dominik Chrastecky <dominik@chrastecky.cz>
2026-08-08 10:16:15 +0200

    Add unlocking with user key

--- cbindings/unlock.go
+++ cbindings/unlock.go
@@ -5,6 +5,10 @@ package main
 */
 import "C"
 import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+
 	"go.chrastecky.dev/bitsailor-core/bitwarden/result"
 )
 
@@ -33,6 +37,39 @@ func BitwardenUnlockSession(
 		setLastError(err)
 		return BitwardenError
 	}
+
+	clearLastError()
+	return BitwardenSuccess
+}
+
+//export BitwardenUnlockSessionWithUserKey
+func BitwardenUnlockSessionWithUserKey(session C.SessionHandle, base64UserKey *C.char) C.BitwardenResult {
+	if base64UserKey == nil {
+		setLastError(nullPointerError("base64UserKey"))
+		return BitwardenError
+	}
+
+	sessionGo, err := getHandleObj[*result.Session](handle(session))
+	if err != nil {
+		setLastError(err)
+		return BitwardenError
+	}
+
+	if sessionGo.Encryption == nil {
+		setLastError(errors.New("the session does not have any encryption data, please make sure to unlock an already populated session"))
+		return BitwardenError
+	}
+
+	userKey, err := base64.StdEncoding.DecodeString(C.GoString(base64UserKey))
+	if err != nil {
+		setLastError(fmt.Errorf("failed base64 decoding the user key: %w", err))
+		return BitwardenError
+	}
+	if len(userKey) != 64 {
+		setLastError(fmt.Errorf("the session expects a 64 byte key, got %d instead", len(userKey)))
+		return BitwardenError
+	}
+	sessionGo.Encryption.UserKey = userKey
 
 	clearLastError()
 	return BitwardenSuccess
